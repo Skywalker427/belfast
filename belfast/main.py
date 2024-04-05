f285373f2359758bc7702951369cb433e4ab117c
Hamzah Bawah <hamzah@motorway.co.uk>
2024-04-05 14:31:35 +0100

    chore: add enum for image_type

--- belfast/main.py
+++ belfast/main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Optional
 
 from fastapi import Response, status
@@ -7,8 +8,15 @@ from .router import app
 from .utils.utils import filter_none
 
 
+class ImageType(str, Enum):
+    v5c = "v5c"
+    id = "id"
+
+
 class RequestBody(BaseModel):
     image: Optional[str] = None
+    image_type: Optional[ImageType] = None
+
 
 class SuccessResponse(BaseModel):
     status: str = "success"
@@ -19,10 +27,18 @@ class ErrorResponse(BaseModel):
     status: str = "error"
     message: str = "An error occurred"
 
+class Enquiry(BaseModel):
+    enquiry_id: str
+    v5c_ocr: Optional[dict] = None
+    id_ocr: Optional[dict] = None
+
 
 @app.post("/upload")
 def upload(body: RequestBody, response: Response):
     image = body.image
+    image_type = body.image_type
+
+    print("--Image type:", image_type)
 
     if image is None:
         error_response = ErrorResponse(
@@ -33,6 +49,7 @@ def upload(body: RequestBody, response: Response):
         return filter_none(error_response.dict())
 
     # Do something with the image
+
 
     res = SuccessResponse(status="success")
     response.status_code = status.HTTP_201_CREATED
